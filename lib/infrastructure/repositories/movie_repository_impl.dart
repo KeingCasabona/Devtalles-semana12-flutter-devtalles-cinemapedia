3c6db230555a9f9f19c7a75ee6855614046d8b3d
KeingCasabona <keing.limache.casabona@gmail.com>
2025-04-29 17:27:54 -0500

    feat: Add MovieRepository implementacion

---
+++ lib/infrastructure/repositories/movie_repository_impl.dart
@@ -0,0 +1,13 @@
+import 'package:cinemapedia/domain/datasources/movies_datasource.dart';
+import 'package:cinemapedia/domain/entities/movie_model.dart';
+import 'package:cinemapedia/domain/repositories/movies_repositories.dart';
+
+class MovieRepositoryImpl extends MoviesRepositories {
+  final MoviesDatasource datasource;
+  MovieRepositoryImpl(this.datasource);
+
+  @override
+  Future<List<MovieModel>> getNowPlayingMovies({int page = 1}) {
+    return datasource.getNowPlayingMovies(page: page);
+  }
+}
